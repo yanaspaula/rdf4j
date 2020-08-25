9fb69b8f897aa26ab11aa3f6487c4fff03c3f3ca
yanaspaula <yanaspaula@gmail.com>
2020-08-25 13:27:01 +0200

    Definition and tests of basic complex datatype functions.

---
+++ core/model/src/main/java/org/eclipse/rdf4j/model/datatypes/ComplexDatatypeUtil.java
@@ -0,0 +1,443 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.model.datatypes;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.complex.ComplexUtils;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.vocabulary.CDT;
+
+/**
+ * Provides methods for handling the cdt:complex datatype.
+ *
+ * @author Yana Soares de Paula
+ */
+public class ComplexDatatypeUtil {
+
+
+	/*-------------------*
+	 * Datatype checking *
+	 *-------------------*/
+
+	/**
+	 * Checks whether the supplied datatype is a main cdt:ucum datatype.
+	 *
+	 * @param datatype
+	 * @return true if the datatype is a main type
+	 */
+	public static boolean isMainDatatype(IRI datatype) {
+		return datatype.equals(CDT.UCUM) || datatype.equals(CDT.UCUMUNIT);
+	}
+
+	/**
+	 * Checks whether the supplied datatype is a specific cdt:ucum datatype. Only complex datatypes are applied in this version.
+	 *
+	 * @param datatype
+	 * @return true if the datatype is a specific type
+	 */
+	public static boolean isSpecificDatatype(IRI datatype) {
+		return datatype.equals(CDT.COMPLEX) || datatype.equals(CDT.COMPLEXCARTESIAN)
+				|| datatype.equals(CDT.COMPLEXPOLAR) 
+//				|| datatype.equals(CDT.AMOUNTOFSUBSTANCE)
+//				|| datatype.equals(CDT.ANGLE) || datatype.equals(CDT.AREA)
+//				|| datatype.equals(CDT.CATALYTICACTIVITY) || datatype.equals(CDT.ACCELERATION)
+//				|| datatype.equals(CDT.DIMENSIONLESS) || datatype.equals(CDT.ELECTRICCAPACITANCE)
+//				|| datatype.equals(CDT.ELECTRICCHARGE) || datatype.equals(CDT.ELECTRICCONDUCTANCE)
+//				|| datatype.equals(CDT.ELECTRICPOTENTIAL) || datatype.equals(CDT.ELECTRICRESISTANCE)
+//				|| datatype.equals(CDT.ENERGY) || datatype.equals(CDT.FORCE)
+//				|| datatype.equals(CDT.FREQUENCY) || datatype.equals(CDT.ILLUMINANCE) || datatype.equals(CDT.LUMINOUSFLUX)
+//				|| datatype.equals(CDT.LENGTH) || datatype.equals(CDT.LUMINOUSINTENSITY)
+//				|| datatype.equals(CDT.MAGNETICFLUX) || datatype.equals(CDT.MAGNETICFLUXDENSITY)
+//				|| datatype.equals(CDT.MASS) || datatype.equals(CDT.POWER)
+//				|| datatype.equals(CDT.PRESSURE) || datatype.equals(CDT.RADIATIONDOSEABSORBED)
+//				|| datatype.equals(CDT.RADIATIONDOSEEFFECTIVE) || datatype.equals(CDT.RADIATIONDOSEEFFECTIVE)
+//				|| datatype.equals(CDT.RADIOACTIVITY) || datatype.equals(CDT.SOLIDANGLE)
+//				|| datatype.equals(CDT.SPEED) || datatype.equals(CDT.TEMPERATURE)
+//				|| datatype.equals(CDT.TIME) || datatype.equals(CDT.VOLUME)
+				;
+	}
+
+	/**
+	 * Checks whether the supplied datatype is a built-in cdt:ucum datatype.
+	 *
+	 * @param datatype
+	 * @return true if it is a main or specific cdt:ucum type
+	 */
+	public static boolean isBuiltInDatatype(IRI datatype) {
+		return isMainDatatype(datatype) || isSpecificDatatype(datatype);
+	}
+
+	/**
+	 * Checks whether the supplied datatype is a complex datatype, including its polar or cartesian derived notations.
+	 *
+	 * @param datatype
+	 * @return true if it is a complex datatype
+	 */
+	public static boolean isComplexDatatype(IRI datatype) {
+		return datatype.equals(CDT.COMPLEX) || isCartesianDatatype(datatype) || isPolarDatatype(datatype);
+	}
+
+	/**
+	 * Checks whether the supplied datatype is equal to the cartesian notation.
+	 *
+	 * @param datatype
+	 * @return true if it is a complexCartesian datatype
+	 */
+	public static boolean isCartesianDatatype(IRI datatype) {
+		return datatype.equals(CDT.COMPLEXCARTESIAN);
+	}
+
+	/**
+	 * Checks whether the supplied datatype is equal to the polar notation.
+	 *
+	 * @param datatype
+	 * @return true if it is a complexPolar type
+	 */
+	public static boolean isPolarDatatype(IRI datatype) {
+		return datatype.equals(CDT.COMPLEXPOLAR);
+	}
+
+	/*----------------*
+	 * Value checking *
+	 *----------------*/
+
+	/**
+	 * Verifies if the supplied lexical value is valid for the given datatype.
+	 *
+	 * @param value    a lexical value
+	 * @param datatype a complex datatatype.
+	 * @return true if the supplied lexical value is valid, false otherwise.
+	 */
+	public static boolean isValidValue(String value, IRI datatype) {
+		boolean result = true;
+
+		if (datatype.equals(CDT.COMPLEX)) {
+			result = isValidComplex(value);
+		} else if (datatype.equals(CDT.COMPLEXCARTESIAN)) {
+			result = isValidCartesian(value);
+		} else if (datatype.equals(CDT.COMPLEXPOLAR)) {
+			result = isValidPolar(value);
+		}
+
+		return result;
+	}
+	
+	/**
+	 * Verifies if the supplied lexical value is a valid decimal or not.
+	 *
+	 * @param value
+	 * @return <tt>true</tt> if valid, <tt>false</tt> otherwise
+	 */
+	public static boolean isValidComplex(String value) {
+		return isValidCartesian(value) || isValidPolar(value);
+	}
+
+	/**
+	 * Verifies if the supplied lexical value is a valid cartesian notation or not.
+	 *
+	 * @param value
+	 * @return <tt>true</tt> if valid, <tt>false</tt> otherwise
+	 */
+	public static boolean isValidCartesian(String value) {
+		try {
+			parseCartesian(value);
+			return true;
+		} catch (IllegalArgumentException e) {
+			return false;
+		}
+	}
+		
+	/**
+	 * Verifies if the supplied lexical value is a valid polar notation or not.
+	 *
+	 * @param value
+	 * @return <tt>true</tt> if valid, <tt>false</tt> otherwise
+	 */
+	public static boolean isValidPolar(String value) {
+		try {
+			parsePolar(value);
+			return true;
+		} catch (IllegalArgumentException e) {
+			return false;
+		}
+	}
+	
+	
+	/*---------------------*
+	 * Value normalization *
+	 *---------------------*/
+
+	/**
+	 * Normalizes the supplied value according to the normalization rules for the supplied datatype.
+	 *
+	 * @param value    The value to normalize.
+	 * @param datatype The value's datatype.
+	 * @return The normalized value if there are any (supported) normalization rules for the supplied datatype, or the
+	 *         original supplied value otherwise.
+	 * @throws IllegalArgumentException If the supplied value is illegal considering the supplied datatype.
+	 */
+	public static String normalize(String value, IRI datatype) {
+		String result = value;
+
+		if (datatype.equals(CDT.COMPLEX)) {
+			result = normalizeComplex(value);
+		} else if (datatype.equals(CDT.COMPLEXCARTESIAN)) {
+			result = normalizeCartesian(value);
+		} else if (datatype.equals(CDT.COMPLEXPOLAR)) {
+			result = normalizePolar(value);
+		} 
+		
+		return result;
+	}
+
+	/**
+	 * Normalizes a complex to its polar or cartesian representations.
+	 *
+	 * @param polar The complex to normalize.
+	 * @return The normalized notation of a complex.
+	 * @throws IllegalArgumentException If one of the supplied strings is not a legal complex.
+	 */
+	public static String normalizeComplex(String complex) {
+			
+		if(isValidCartesian(complex)) {
+			return normalizeCartesian(complex);
+		} else if(isValidPolar(complex)) {
+			return normalizePolar(complex);
+		} else {
+			throw new IllegalArgumentException("Not a legal complex value:" + complex);
+		}
+	}
+	
+
+	/**
+	 * Normalizes a complex to its cartesian representation according to the 
+	 * org.apache.commons.math4.complex.Complex library.
+	 *
+	 * @param polar The cartesian to normalize.
+	 * @return String (real, imaginary)
+	 * @throws IllegalArgumentException If one of the supplied strings is not a legal cartesian.
+	 */
+	private static String normalizeCartesian(String value) {
+		Double real = 0.0;
+		Double imaginary = 0.0;
+		Complex complex = parseCartesian(value);
+		String result;
+		
+		real = complex.getReal();
+		imaginary = complex.getImaginary();
+		
+		if(imaginary.toString().startsWith("-")) {
+			result = (real.toString() + " " + imaginary.toString().replaceFirst("-", "- ") + "i");
+		} else {
+			result = (real.toString() + " + " +  imaginary.toString().replace("+", "") + "i");
+		}
+		
+		return result;
+		  
+	}
+	
+	/**
+	 * Normalizes a complex to its polar representation, concerning its pattern proposed in P_POLAR.
+	 *
+	 * @param polar The polar to normalize.
+	 * @return The polar notation of a complex.
+	 * @throws IllegalArgumentException If one of the supplied strings is not a legal polar.
+	 */
+	public static String normalizePolar(String value) {
+		return parsePolar(value).toString();
+	}
+	
+	/*---------------*
+	 * Value parsing 
+	 *---------------*/
+	
+	/**
+	 * Parses the supplied complex string and returns its value.
+	 *
+	 * @param s A string representation of a complex value.
+	 * @return The <tt>complex</tt> value represented by the supplied string argument.
+	 * @throws NumberFormatException If the supplied string is not a valid complex value.
+	 */
+	public static Complex parseComplex(String s) {
+		Complex complex = new Complex(0.0);
+		
+		if(isValidCartesian(s)) {
+			complex = parseCartesian(s);
+		} else if(isValidPolar(s)) {
+			complex = parsePolar(s);		
+		} else {
+			throwIAE(s + " is NOT a valid complex notation !");
+		}
+		
+		return complex;
+	}
+	
+	/**
+	 * Parses the supplied complex cartesian string and returns its value.
+	 *
+	 * @param s A string representation of an xsd:boolean value.
+	 * @return The <tt>boolean</tt> value represented by the supplied string argument.
+	 * @throws NumberFormatException If the supplied string is not a valid complex cartesian notation.
+	 */
+	public static Complex parseCartesian(String value) {
+		Double re = 0.0;
+		Double im = 0.0;
+		
+		value = value.replaceAll("\\p{Space}+", "");
+		
+		// Groups of decimals
+		final String real = "\\p{Digit}+(?:\\.\\p{Digit}*)?(?:[eE][+-]?\\p{Digit}+)?";
+		final String imaginary = "\\p{Digit}+(?:\\.\\p{Digit}*)?(?:[eE][+-]?\\p{Digit}+)?";
+		
+		// RegEx to cartesian complex notations
+		final String cartesian = 
+				// Cartesian notations accepted
+				("(([+-]?" + real + ")" + "([+-]" + imaginary + "[ij]))|" +	// ex : -7 + 4j
+				"(([+-]?" + real + ")" + "([+-][ij]" + imaginary + "))|" +	// ex : -7 + i4
+				"(([+-]?[ij]" + imaginary + ")" + "([+-]" + real + "))|" +	// ex : i4 - 7
+				"(([+-]?" + imaginary + "[ij])" + "([+-]" + real + "))|" +	// ex : 4j - 7
+				
+				// Pure imaginary number
+				"([+-]?[ij]" + imaginary + ")|" + 
+				"([+-]?" + imaginary + "[ij])|" + 
+				
+				// Pure real number
+				"([+-]?" + real + ")"); 
+		
+		Pattern pattern = Pattern.compile(cartesian);
+		Matcher matcher = pattern.matcher(value);
+		
+		// Separates real and imaginary parts of the string into doubles
+		if (Pattern.matches(cartesian, value)) {
+			while (matcher.find()) {
+				// Group 1:  [+-] {real} [+-] {imaginary}
+				if(matcher.group(1) != null) {
+					re = Double.parseDouble(matcher.group(2));
+					im = Double.parseDouble(matcher.group(3).replaceAll("[ij]", ""));
+				}
+				// Group 4:  [+-] {real} [+-] {imaginary}
+				if(matcher.group(4) != null) {
+					re = Double.parseDouble(matcher.group(5));
+					im = Double.parseDouble(matcher.group(6).replaceAll("[ij]", ""));
+				}
+				// Group 7:  [+-] {imaginary} [+-] {real} 
+				if(matcher.group(7) != null) {
+					im = Double.parseDouble(matcher.group(8).replaceAll("[ij]", ""));
+					re = Double.parseDouble(matcher.group(9));
+				}
+				// Group 10:  [+-] {imaginary} [+-] {real} 
+				if(matcher.group(10) != null) {
+					im = Double.parseDouble(matcher.group(11).replaceAll("[ij]", ""));
+					re = Double.parseDouble(matcher.group(12));
+				}
+				// Group 13: =  [+-] {imaginary}
+				if(matcher.group(13) != null) 
+					im = Double.parseDouble(matcher.group(13).replaceAll("[ij]", ""));
+				
+				// Group 14:  [+-] {imaginary}
+				if(matcher.group(14) != null) 
+					im = Double.parseDouble(matcher.group(14).replaceAll("[ij]", ""));
+				
+				// Group 15:  [+-] {real} 
+				if(matcher.group(15) != null) 
+					re = Double.parseDouble(matcher.group(15));
+			  }
+		  }	else 
+			  throwIAE(value + " is NOT a valid cartesian notation !");
+		
+		return new Complex(re, im);
+	}
+	
+	/**
+	 * Parses the supplied complex polar string and returns its value.
+	 *
+	 * @param s A string representation of an polar complex value.
+	 * @return The <tt>complex</tt> value represented by the supplied string argument.
+	 * @throws NumberFormatException If the supplied string is not a valid complex polar notation.
+	 */
+	public static Complex parsePolar(String value) {
+		double r = 0.0;
+		double theta = 0.0; // in radians
+		
+		value = value.replaceAll("\\p{Space}+", "");
+		
+		// Groups of decimals
+		final String module = "\\p{Digit}+(?:\\.\\p{Digit}*)?(?:[eE][+-]?\\p{Digit}+)?";
+		final String angle = "\\p{Digit}+(?:\\.\\p{Digit}*)?(?:[eE][+-]?\\p{Digit}+)?";
+		
+		// RegEx to polar complex notations
+		final String polar = "\\((" + module + ")[;,]([+-]?" + angle + ")((?:\\[pi\\])?\\[(?:rad|deg)\\])\\)";
+		
+		Pattern pattern = Pattern.compile(polar);
+		Matcher matcher = pattern.matcher(value);
+		
+		// Separates module and angle parts of the string into doubles
+		if (Pattern.matches(polar, value)) {
+			while (matcher.find()) {
+				r = Double.parseDouble(matcher.group(1));
+			
+			if (matcher.group(3).contains("[pi][rad]")) {
+				theta = Double.parseDouble(matcher.group(2));
+			} else if(matcher.group(3).contains("[deg]") && !matcher.group(3).contains("[pi]")) {
+				// Transforms degrees in radians
+				theta = Double.parseDouble(matcher.group(2));
+				theta = Math.toRadians(theta);
+			} else {
+				throwIAE(value + " is NOT a valid cartesian notation !");
+				}
+			}
+		} else 
+			throwIAE(value + " is NOT a valid cartesian notation !");
+		
+		return ComplexUtils.polar2Complex(r, theta);
+	}
+
+
+	/*------------------*
+	 * Value comparison *
+	 *------------------*/
+
+	
+	public static boolean compare(String value1, String value2, IRI datatype) {
+		if (datatype.equals(CDT.COMPLEX) || datatype.equals(CDT.COMPLEXCARTESIAN) || datatype.equals(CDT.COMPLEXPOLAR))
+			return compareComplex(value1, value2);
+		else
+			throw new IllegalArgumentException("datatype is not ordered");
+	}
+
+	/**
+	 * Compares two complexes to each other.
+	 *
+	 * @param complex1
+	 * @param complex2
+	 * @return Returns true if <tt>complex1</tt> is equal to <tt>complex2</tt> or return false
+	 *         if <tt>complex1</tt> differs from <tt>complex2</tt>.
+	 * @throws IllegalArgumentException If one of the supplied strings is not a legal complex.
+	 */
+	public static boolean compareComplex(String complex1, String complex2) {
+		Complex comp1 = parseComplex(complex1);
+		Complex comp2 = parseComplex(complex2);
+
+		return Complex.equals(comp1, comp2);
+	}
+	
+
+	/*-----------------*
+	 * Utility methods *
+	 *-----------------*/
+	
+	/**
+	 * Throws an IllegalArgumentException that contains the supplied message.
+	 */
+	private static final void throwIAE(String msg) {
+		throw new IllegalArgumentException(msg);
+	}
+}
---
+++ core/model/src/test/java/org/eclipse/rdf4j/model/datatypes/ComplexDatatypeUtilTest.java
@@ -0,0 +1,121 @@
+package org.eclipse.rdf4j.model.datatypes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.vocabulary.CDT;
+import org.junit.Test;
+
+/**
+ * Unit tests on {@link org.eclipse.rdf4j.model.datatypes.ComplexDatatypeUtil}
+ * 
+ * @author Yana Soares de Paula
+ */
+public class ComplexDatatypeUtilTest {
+
+	private static final String[] VALID_CARTESIANS = { 
+			"4.0+j7",
+			"5 - 8j",
+			"3 + i9", 
+			"2.0 - 7i", 
+			"-7i +		8",
+			"+i2 - 6.0", 
+			"-j5+0.00001", 
+			"-j5+ 2e2", 
+			"7j - 2",
+			"+7i",
+			"-34",
+			"20",
+			"4e5 + 1j"};
+	
+	private static final String[] INVALID_CARTESIANS = { 
+			"4.0++j7",
+			"5 -8j-",
+			"3,908e3 + i9", 
+			"2,0 - 7i", 
+			"-7ii +		8",
+			"+i2j - 6.0", 
+			"-j5+0.00001e", 
+			"-j5+2+e2", 
+			"e7j - 2",
+			"+7ib",
+			"-34+0",
+			"-+20",
+			"aaaaaaaaa 	aaaa"};
+	
+	private static final String[] VALID_POLARS = { 
+			"(3e5; 	90[deg])",
+			"(8.6, 3.245 [pi]	[rad])",
+			"(1.85	;	232 [deg])", 
+			"(35,  -3[pi][rad])"};
+	
+	private static final String[] INVALID_POLARS = { 
+			"(3e5; 	90[deg][rad])",
+			"(8.6, 324.5 [pi]	[pi][rad])",
+			"(1.85[rad]	;	232)", 
+			"(35,  -180[pi][deg])",
+			"35, [pi][rad]",
+			"5-20, 50[deg]°"};
+
+	/**
+	 * Test method for
+	 * {@link org.eclipse.rdf4j.model.datatypes.ComplexDatatypeUtil#isValidValue(java.lang.String, org.eclipse.rdf4j.model.IRI)}
+	 * .
+	 */
+	@Test
+	public void testIsValidValue() {
+
+		testValidation(VALID_CARTESIANS, CDT.COMPLEXCARTESIAN, true);		
+		testValidation(INVALID_CARTESIANS, CDT.COMPLEXCARTESIAN, false);
+		
+		testValidation(VALID_POLARS, CDT.COMPLEXPOLAR, true);		
+		testValidation(INVALID_POLARS, CDT.COMPLEXPOLAR, false);
+	}
+	
+	private void testValidation(String[] values, IRI datatype, boolean validValues) {
+		for (String value : values) {
+			boolean result = ComplexDatatypeUtil.isValidValue(value, datatype);
+			if (validValues) {
+				if (!result) {
+					fail("value " + value + " should have validated for type " + datatype);
+				}
+			} else {
+				if (result) {
+					fail("value " + value + " should not have validated for type " + datatype);
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void testNormalize() {
+		assertEquals("8.0 - 7.0i", ComplexDatatypeUtil.normalize("-7i +		8", CDT.COMPLEXCARTESIAN));
+		assertEquals("1.0E-5 - 5.0i", ComplexDatatypeUtil.normalize("-j5+0.00001", CDT.COMPLEXCARTESIAN));
+		assertEquals("-34.0 + 0.0i", ComplexDatatypeUtil.normalize("-34", CDT.COMPLEXCARTESIAN));
+		assertEquals("5.0 + 3.0i", ComplexDatatypeUtil.normalize("5 + 3j", CDT.COMPLEXCARTESIAN));
+		assertEquals("5.0 + 8.0E-7i", ComplexDatatypeUtil.normalize("5   + 8e-7j", CDT.COMPLEXCARTESIAN));
+		assertEquals("-0.005 + 8.0i", ComplexDatatypeUtil.normalize("+ 8j - 5e-3   ", CDT.COMPLEXCARTESIAN));
+		
+		assertEquals("(30.021011266294206, 39.98423292435452)", ComplexDatatypeUtil.normalize("(50,53.1[deg])", CDT.COMPLEXPOLAR));
+	}
+	
+	@Test
+	public void testParseComplex() {
+		for (String value : VALID_CARTESIANS) {
+			ComplexDatatypeUtil.parseComplex(value);
+		}
+		
+		for (String value : VALID_POLARS) {
+			ComplexDatatypeUtil.parseComplex(value);
+		}
+	}
+	
+	@Test
+	public void testCompareComplex() {
+		ComplexDatatypeUtil.compareComplex("-j5+0.00001", "1.0e-5 - 5.0i");
+		ComplexDatatypeUtil.compareComplex("(3; 	180[deg])", "(3.0 , 1 [pi][rad])");
+		
+	}
+	
+}
